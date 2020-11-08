33d2d1def58b23c1cd9bec5491b4c06c109035f3
galenbr <gibrown@wpi.edu>
2020-11-08 10:05:08 -0500

    Moved old calculations

--- src/scarabot/scarabot_fk/src/fk_service_client.py
+++ src/scarabot/scarabot_fk/src/fk_service_client.py
@@ -4,7 +4,39 @@ import rospy
 from gazebo_msgs.srv import GetJointProperties, GetJointPropertiesRequest
 from geometry_msgs.msg import Pose
 import numpy as np
+import math
 
+l1_r = 0.615
+l1_z = 1.135
+l2 = 0.4
+l3 = 0.3
+z_start = 0.835
+
+def homogenousTransform(jointValues):
+    [q1, q2, q3] = jointValues
+    ret = np.array([[1,0,0,0],
+                    [0,1,0,0],
+                    [0,0,1,0],
+                    [0,0,0,1]])
+    revolute_link1_transform = np.array([
+        [math.cos(q1),-math.sin(q1),0,l1_r*math.cos(q1)],
+        [math.sin(q1), math.cos(q1),0,l1_r*math.sin(q1)],
+        [0,0,1,l1_z],
+        [0,0,0,1]])
+    ret = np.matmul(ret, revolute_link1_transform)
+    revolute_link2_transform = np.array([
+        [math.cos(q2), math.sin(q2),0,l2*math.cos(q2)],
+        [math.sin(q2), -math.cos(q2),0,l2*math.sin(q2)],
+        [0,0,-1,0],
+        [0,0,0,1]])
+    ret = np.matmul(ret, revolute_link2_transform)
+    prismatic_link_transform = np.array([
+        [1,0,0,0],
+        [0,1,0,0],
+        [0,0,1,l3+q3],
+        [0,0,0,1]])
+    ret = np.matmul(ret, prismatic_link_transform)
+    return ret
 
 class FkService:
     def __init__(self):
@@ -17,17 +49,18 @@ class FkService:
     def read_joint_value(self, joint_name):
         self.object_name.joint_name = joint_name
         result = self.fk_service(self.object_name)
-        return result.position
+        return result.position[0]
 
     def cal_end_effector_pose(self):
-        theta1 = self.read_joint_value("revolute_joint_1")
-        theta2 = self.read_joint_value("revolute_joint_2")
-        d = self.read_joint_value("joint_prismatic")
-        self.eepose.position.x = np.float64(theta1)
-        self.eepose.position.y = np.float64(theta2)
-        self.eepose.position.z = np.float64(d)
+        theta1 = self.read_joint_value("revolute_cylinder1")
+        theta2 = self.read_joint_value("revolute_cylinder2")
+        d = self.read_joint_value("prismatic_arm")
+        homogenousMatrix = homogenousTransform([theta1, theta2, d])
+        self.eepose.position.x = np.float64(homogenousMatrix[0, 3])
+        self.eepose.position.y = np.float64(homogenousMatrix[1, 3])
+        self.eepose.position.z = np.float64(homogenousMatrix[2, 3])
         self.pub.publish(self.eepose)
-        rospy.loginfo("The End Effector pose has been published")
+        rospy.loginfo(self.eepose)
 
 
 if __name__ == '__main__':
